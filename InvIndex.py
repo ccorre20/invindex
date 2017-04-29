a7bff94d92ead087c732d0a8d8fb70112f92c6a4
Camilo Correa Restrepo <ccorre20@master.(none)>
2017-04-29 18:26:03 -0400

    first commit

---
+++ InvIndex.py
@@ -0,0 +1,50 @@
+from mrjob.job import MRJob
+from pymongo import MongoClient
+import re
+import string
+import os
+
+client = MongoClient('mongodb://10.131.137.188:27017')
+db = client['grupo_03']
+db.authenticate('user1','eafit.2017')
+tests = db.tests
+
+class InvIndex(MRJob):
+
+	def mapper(self, _, line):
+		line = re.sub(r'\d+', '', line)
+		line = line.translate(string.maketrans("",""), string.punctuation)
+		words = line.split()
+		try:
+    			file_in = os.environ['mapreduce_map_input_file']
+		except KeyError:
+    			file_in = os.environ['map_input_file']
+		for word in words:
+			yield word.lower()+'+'+file_in, 1
+
+	def combiner(self, key, values):
+		s = key.split('+')
+		freq = sum(values)
+		yield s[0], s[1]+'+'+str(freq)
+
+	def reducer(self, key, values):
+		files = []
+		freqs = []
+		for v in values:
+			s = v.split('+')
+			try:
+				i = files.index(s[0])
+			except ValueError:
+				files.append(s[0])
+				freqs.append(s[1])
+			else:
+				x = int(freqs[i])
+				y = int(s[1])
+				freqs[i] = str(x+y)
+		tuples = zip(files,freqs)
+		tests.insert_one({'_id': key, 'data':  tuples})
+		yield key, tuples
+	
+		
+if __name__ == '__main__':
+	InvIndex.run()	
---
+++ README.md
@@ -0,0 +1,1 @@
+This repository contains a python script designed to interpret a collection of texts and create an index based on word frequency for subsequent analysis and use.
